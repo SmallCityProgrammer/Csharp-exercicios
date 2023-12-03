24601169fa3f8130c177925f959e30c387bed9f3
Eliezer Castro <smallcity.programmer@gmail.com>
2023-12-03 19:31:08 -0300

    Exercicio 2

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Empresa
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] votos = new int[6];
+      string[] sistemas = { "Windows Server", "Unix", "Linux", "Netware", "Mac OS", "Outro" };
+      int totalVotos = 0;
+
+      Console.WriteLine("Qual o melhor Sistema Operacional para uso em servidores?");
+      Console.WriteLine("As possíveis respostas são:");
+      for (int i = 0; i < sistemas.Length; i++)
+      {
+        Console.WriteLine($"{i + 1}- {sistemas[i]}");
+      }
+      Console.WriteLine("Digite 0 para encerrar a votação.");
+
+      while (true)
+      {
+        Console.Write("Voto: ");
+        string entrada = Console.ReadLine();
+        int voto;
+
+        if (int.TryParse(entrada, out voto))
+        {
+          if (voto >= 1 && voto <= 6)
+          {
+            votos[voto - 1]++;
+            totalVotos++;
+          }
+          else if (voto == 0)
+          {
+            break;
+          }
+          else
+          {
+            Console.WriteLine("Opção inválida. Por favor, digite um número de 1 a 6 ou 0 para encerrar.");
+          }
+        }
+        else
+        {
+          Console.WriteLine("Entrada inválida. Por favor, digite um número de 1 a 6 ou 0 para encerrar.");
+        }
+      }
+
+      Console.WriteLine("Sistema Operacional   Votos   %");
+      Console.WriteLine("------------------- ----- ---");
+
+      for (int i = 0; i < sistemas.Length; i++)
+      {
+        double percentual = (double)votos[i] / totalVotos * 100;
+        Console.WriteLine($"{sistemas[i],-20} {votos[i],-7} {percentual:F0}%");
+      }
+
+      Console.WriteLine("------------------- ----- ---");
+      Console.WriteLine($"Total {totalVotos}");
+
+      int maxVotos = 0;
+      int indiceVencedor = 0;
+
+      for (int i = 0; i < sistemas.Length; i++)
+      {
+        if (votos[i] > maxVotos)
+        {
+          maxVotos = votos[i];
+          indiceVencedor = i;
+        }
+      }
+
+      Console.WriteLine($"O Sistema Operacional mais votado foi o {sistemas[indiceVencedor]}, com {votos[indiceVencedor]} votos, correspondendo a {(double)maxVotos / totalVotos * 100:F0}% dos votos.");
+    }
+  }
+}
